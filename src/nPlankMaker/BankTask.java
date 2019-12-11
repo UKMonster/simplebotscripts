60988ba951aff765ef2b21dc012ca9301f18ac8c
UKMonster <nathanadsmith1@gmail.com>
2019-12-11 19:01:24 +0000

    final

--- src/nPlankMaker/BankTask.java
+++ src/nPlankMaker/BankTask.java
@@ -23,21 +23,27 @@ public class BankTask extends Task {
 
 	@Override
 	public void run() {
-		if(!ctx.bank.bankOpen()){
-			SimpleObject depositbox = ctx.objects.populate().filter("Grand Exchange Booth","bank booth").nextNearest();
-			if(depositbox != null && depositbox.validateInteractable()){
-				if (depositbox.click("Bank") && ctx.onCondition(() -> ctx.bank.bankOpen(),300,10)) {
-					ctx.bank.depositAllExcept("Nature rune", "Astral rune", "Earth rune", "Coins","Rune pouch");
-					SimpleItem planksB = ctx.bank.populate().filter(plankName).next();
-					if(planksB != null && planksB.click("withdraw-all")&& ctx.onCondition(() -> !ctx.inventory.populate().filter(plankName).isEmpty(),300,10)){
-						ctx.bank.closeBank();
-					} else if(planksB == null){
-						ctx.stopScript();
-					}
-				}
-			}
-		}
-	}
+		 if(!ctx.bank.bankOpen()){
+	            SimpleObject depositbox = ctx.objects.populate().filter("Grand Exchange Booth","bank booth").nextNearest();
+	            if(depositbox != null && depositbox.validateInteractable()){
+	                if (depositbox.click("Bank") && ctx.onCondition(() -> ctx.bank.bankOpen(),300,10)) {
+	                    bankItems();
+	                }
+	            }
+	        } else {
+	            bankItems();
+	        }
+	    }
+
+	    private void bankItems() {
+	        ctx.bank.depositAllExcept("Nature rune", "Astral rune", "Earth rune", "Coins","Rune pouch");
+	        SimpleItem planksB = ctx.bank.populate().filter(plankName).next();
+	        if(planksB != null && planksB.click("withdraw-all")&& ctx.onCondition(() -> !ctx.inventory.populate().filter(plankName).isEmpty(),300,10)){
+	            ctx.bank.closeBank();
+	        } else if(planksB == null){
+	            ctx.stopScript();
+	        }
+	    }
 	@Override
 	public String status() {
 		// TODO Auto-generated method stub
